561c716bac81e24907340043f5a3916caad53f9e
Lestár Patrik <lrpatrik55@gmail.com>
2020-06-24 12:51:15 +0200

    Version 0.1

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
 $(document).ready(function(){
-  $('img').click(function(){
-    $('body').css('background-color', 'black');
-    $('button').show();
-    $('h1').text('BLM').css('color', 'white');
-    $(this).hide();
-  });
-  $('button').click(function(){
-    $(this).hide();
-    $('body').css('background-color', 'white');
-    $('h1').text('Ez az oldal jelenleg készítés alatt áll...').css('color', 'black');
-    $('img').show();
+  $('.notready').click(function(){
+    alert('Figyelem! Ez az oldal még nem készült el!');
   });
 });
