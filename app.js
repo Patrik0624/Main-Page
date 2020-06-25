bbbf223f7897065e6c3ad64e5eb55fe4a4de67f1
Lestár Patrik <lrpatrik55@gmail.com>
2020-06-25 10:24:10 +0200

    Version 0.2 - bug fixes

--- app.js
+++ app.js
@@ -2,4 +2,15 @@ $(document).ready(function(){
   $('.notready').click(function(){
     alert('Figyelem! Ez az oldal még nem készült el!');
   });
+  window.onscroll = function() {scrollFunction()};
+  function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+      $('.scrollbutton').fadeIn();
+    } else {
+      $('.scrollbutton').fadeOut();
+    };
+  };
+  $('.scrollbutton').click(function(){
+    $("html, body").animate({scrollTop: 0}, "slow");
+  });
 });
